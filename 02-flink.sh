cb2c429d0b8ad094f63a8b7d7a77c429feb72e24
Robert Young <robeyoun@redhat.com>
2024-07-01 11:53:55 +1200

    Fix gateway config and add script to start gateway sql session

--- 02-flink.sh
+++ 02-flink.sh
@@ -12,7 +12,7 @@ echo "waiting for kafka to start, may take some time to download down images"
 kubectl wait kafka/my-cluster --for=condition=Ready --timeout=700s
 git checkout kafka.yaml
 echo "creating transactions topic, may take some time to download kafka image"
-kubectl run client -i --image quay.io/strimzi/kafka:latest-kafka-3.7.0 --restart=Never --rm --pod-running-timeout 5m0s -- bin/kafka-topics.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic transactions --partitions 1 --create
+kubectl run client -i --image quay.io/strimzi/kafka:latest-kafka-3.7.0 --restart=Never --rm --pod-running-timeout 5m0s -- bin/kafka-topics.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic transactions --partitions 3 --create
 echo "building fraud-detection image in minikube"
 minikube image build . -t fraud-detection:latest
 echo "applying flink fraud detection deployment"
